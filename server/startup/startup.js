b942fc51b4ee5a3c3451df50abd40417323ec577
vagrant <vasrush@gmail.com>
2019-04-24 12:29:09 +0000

    Import appEvents

--- server/startup/startup.js
+++ server/startup/startup.js
@@ -1,6 +1,7 @@
 import Reaction from "/imports/plugins/core/core/server/Reaction";
+import appEvents from "/imports/node-app/core/util/appEvents";
 
-appEvents.emit("afterCoreInit", () => {
+appEvents.on("afterCoreInit", () => {
   Reaction.addRolesToGroups({
     allShops: true,
     groups: ["customer", "guest"],
